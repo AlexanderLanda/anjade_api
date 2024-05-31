314c237b75c7937ecf5116d0a68eb4ece636547c
Alexander Landa <alanda@acid.cl>
2024-05-31 18:39:51 +0200

    cors localhost

--- src/main/java/com/anjade/config/SecurityConfig.java
+++ src/main/java/com/anjade/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
 	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 	        CorsConfiguration config = new CorsConfiguration();
 	        config.setAllowCredentials(true);
+	        config.addAllowedOrigin("http://localhost:4200");
 	        config.addAllowedOrigin("http://localhost:8080");
 	        config.addAllowedOrigin("https://anjade.es");
 	        config.addAllowedHeader("*");
