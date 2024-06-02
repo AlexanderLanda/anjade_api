45a201c3d72458e29bf8b854ee433144c40e4391
Alexander Landa <alanda@acid.cl>
2024-06-02 18:01:54 +0200

    probando pase a redsys prod

--- src/main/java/com/anjade/controller/RedsysController.java
+++ src/main/java/com/anjade/controller/RedsysController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "${frontend.url}")
+//@CrossOrigin(origins = "${frontend.url}")
 public class RedsysController {
 	
 	@Value("${frontend.url}") // Inyecta el valor de frontend.url
