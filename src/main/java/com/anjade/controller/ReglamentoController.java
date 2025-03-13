39f8ba27b3c406d990494cb1a21a64800005e60e
Alexander Landa <alanda@acid.cl>
2025-03-13 01:10:49 +0100

    realgmentos a la nube y bd

--- src/main/java/com/anjade/controller/ReglamentoController.java
+++ src/main/java/com/anjade/controller/ReglamentoController.java
@@ -1,9 +1,11 @@
 package com.anjade.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,11 +14,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.anjade.entity.AttachmentDto;
+import com.anjade.entity.DeportesDto;
 import com.anjade.entity.ReglamentosDto;
+import com.anjade.entity.ReportDto;
 import com.anjade.service.ReglamentoService;
 import com.anjade.serviceImpl.ReglamentoServiceImpl;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @RequestMapping("/api/v1/reglamentos")
@@ -38,8 +47,28 @@ public class ReglamentoController {
 
 
     @PostMapping
-    public ReglamentosDto saveReglamento(@RequestBody ReglamentosDto reglamento) {
-        return service.saveReglamento(reglamento);
+    public List<ReglamentosDto> saveReglamento( @RequestPart("deporte") String deporte, @RequestPart(value = "files", required = false) List<MultipartFile> files) {
+    	
+    	List<ReglamentosDto> reglamentos = new ArrayList<ReglamentosDto>();
+    	ObjectMapper objectMapper = new ObjectMapper();
+		try {
+		
+    	
+    	for (MultipartFile file : files) {
+    		ReglamentosDto reglamento = new ReglamentosDto();
+    		reglamento.setName(file.getOriginalFilename());
+    		reglamento.setType(file.getContentType());
+    		reglamento.setPath("/ficheros/documentos/reglamentos_deportivos/"+file.getOriginalFilename());
+            reglamento.setDeporte(deporte);
+            reglamentos.add(reglamento);
+            service.saveReglamento(reglamento);
+            
+
+        }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return reglamentos;
     }
 
     @DeleteMapping("/{id}")
